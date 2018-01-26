1ee27fb2149383328789837e4267c96258d2fc62
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-01-26 11:12:51 +0100

    lodel works again

--- .docker/lodel/db/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh -N -i /ssh/id_rsa -F /ssh/config -o UserKnownHostsFile=/ssh/known_hosts -L 172.31.0.30:3306:localhost:3306 calenda-devel
--- .docker/lodel/fpm/entrypoint.sh
+++ .docker/lodel/fpm/entrypoint.sh
@@ -3,30 +3,30 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-adduser -D -s /bin/false -u $USER_ID user
+
 
 cp /code/lodelconfig-default.php /code/lodelconfig.php
 sed -i "s|exit()|// exit()|g" /code/lodelconfig.php
 sed -i "s/^\$cfg\['database'\] = '';$/\$cfg['database'] = '$MYSQL_DATABASE';/g" /code/lodelconfig.php
 sed -i "s/^\$cfg\['dbusername'\] = '';$/\$cfg['dbusername'] = '$MYSQL_USER';/g" /code/lodelconfig.php
 sed -i "s/^\$cfg\['dbpasswd.*$/\$cfg['dbpasswd'] = '$MYSQL_PASSWORD';/g" /code/lodelconfig.php
-sed -i "s/^\$cfg\['dbhost.*$/\$cfg['dbhost'] = '172.31.0.30';/g" /code/lodelconfig.php
+sed -i "s/^\$cfg\['dbhost.*$/\$cfg['dbhost'] = 'db';/g" /code/lodelconfig.php
 sed -i "s/^\$cfg\['debugMode.*$/\$cfg['debugMode'] = $DEBUG_MODE;/g" /code/lodelconfig.php
 
-sed -i "s/;security.limit_extensions = .php .php3 .php4 .php5/security.limit_extensions = .php .html/" /usr/local/etc/php-fpm.d/www.conf
+if [ $DEBUG_MODE == 2 ]; then
+	sed -i "s/log_level = notice/log_level = debug/" /usr/local/etc/php-fpm.d/www.conf
+fi
 
 
 chmod 664 /code/lodelconfig.php
 chown $USER_ID:$USER_ID /code/lodelconfig.php
 
-composer update -d /code
+composer install -d /code/lodel/scripts/
 
 keyfile=$(sed -n 's/\$cfg\['\''install_key'\''] = '\''\(.*\)'\'';/\1/p' /code/lodelconfig.php)
-echo $keyfile
+
 touch /code/$keyfile
 chmod 664 /code/$keyfile
 chown $USER_ID:$USER_ID /code/$keyfile
-
-
 
 exec php-fpm
